23b12d8d44aa04be3e166ee8cbce06b6e4b9ad16
AndreaRispoli97 <Andrea9715@outlook.it>
2025-08-09 11:35:45 +0200

    Add: Complete Step 3-4-5(Bonus)

--- src/main/java/org/lessons/java/spring/best_of_the_year/controller/HomeController.java
+++ src/main/java/org/lessons/java/spring/best_of_the_year/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequestMapping("/")
@@ -25,32 +26,62 @@ public class HomeController {
     @GetMapping("/movies")
     public String movies(Model model) {
         List<Movie> movies = getBestMovies();
+        model.addAttribute("movies", movies);
 
-        String movieTitles = "";
-        for (int i = 0; i < movies.size(); i++) {
-            movieTitles += movies.get(i).getTitle();
-            if (i < movies.size() - 1) {
-                movieTitles += ", ";
-            }
-        }
-        model.addAttribute("movieList", movieTitles);
+        // String movieTitles = "";
+        // for (int i = 0; i < movies.size(); i++) {
+        // movieTitles += movies.get(i).getTitle();
+        // if (i < movies.size() - 1) {
+        // movieTitles += ", ";
+        // }
+        // }
+        // model.addAttribute("movieList", movieTitles);
+
         return "movies";
     }
 
     @GetMapping("/songs")
     public String songs(Model model) {
         List<Song> songs = getBestSongs();
+        model.addAttribute("songs", songs);
 
-        String songTitles = "";
-        for (int i = 0; i < songs.size(); i++) {
-            songTitles += songs.get(i).getTitle();
-            if (i < songs.size() - 1) {
-                songTitles += ", ";
-            }
-        }
-        model.addAttribute("songList", songTitles);
+        // String songTitles = "";
+        // for (int i = 0; i < songs.size(); i++) {
+        // songTitles += songs.get(i).getTitle();
+        // if (i < songs.size() - 1) {
+        // songTitles += ", ";
+        // }
+        // }
+        // model.addAttribute("songList", songTitles);
 
         return "songs";
+    }
+
+    @GetMapping("/movies/{id}")
+    public String movie(@PathVariable int id, Model model) {
+        List<Movie> movies = getBestMovies();
+        for (Movie movie : movies) {
+            if (movie.getId() == id) {
+                model.addAttribute("movieTitle", movie.getTitle());
+                break;
+            }
+        }
+
+        return "movie-detail";
+    }
+
+    @GetMapping("/songs/{id}")
+    public String song(@PathVariable int id, Model model) {
+        List<Song> songs = getBestSongs();
+        for (Song song : songs) {
+            if (song.getId() == id) {
+                model.addAttribute("songTitle", song.getTitle());
+                break;
+            }
+        }
+
+        return "song-detail";
+
     }
 
     private List<Movie> getBestMovies() {
